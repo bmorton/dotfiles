31638a2f854c36929691ace6e53072b5a6f61f90
Brian Morton <bmorton@sdreader.com>
2011-09-14 23:57:44 -0700

    Add install script for dotfiles.

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/rvmrc ~/.rvmrc
-ln -s ~/.dotfiles/gemrc ~/.gemrc
+echo 'Creating symlinks...'
+rm ~/.bashrc && ln -s ~/.dotfiles/bash.sh ~/.bashrc
+rm ~/.bash_profile && ln -s ~/.dotfiles/bash.sh ~/.bash_profile
+rm ~/.gitconfig && ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+rm ~/.rvmrc && ln -s ~/.dotfiles/rvmrc ~/.rvmrc
+rm ~/.gemrc && ln -s ~/.dotfiles/gemrc ~/.gemrc
+rm ~/.CFUserTextEncoding && ln -s ~/.dotfiles/CFUserTextEncoding ~/.CFUserTextEncoding
+echo 'Done!'
