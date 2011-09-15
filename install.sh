1c3be8a2eac577b3c64bc886bd3257161f45678c
Brian Morton <bmorton@sdreader.com>
2011-09-15 00:04:50 -0700

    Add bashmarks configuration.

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 echo 'Creating symlinks...'
-rm ~/.bashrc && ln -s ~/.dotfiles/bash.sh ~/.bashrc
-rm ~/.bash_profile && ln -s ~/.dotfiles/bash.sh ~/.bash_profile
-rm ~/.gitconfig && ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-rm ~/.rvmrc && ln -s ~/.dotfiles/rvmrc ~/.rvmrc
-rm ~/.gemrc && ln -s ~/.dotfiles/gemrc ~/.gemrc
-rm ~/.CFUserTextEncoding && ln -s ~/.dotfiles/CFUserTextEncoding ~/.CFUserTextEncoding
+rm ~/.bashrc
+ln -s ~/.dotfiles/bash.sh ~/.bashrc
+
+rm ~/.bash_profile
+ln -s ~/.dotfiles/bash.sh ~/.bash_profile
+
+rm ~/.gitconfig
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+
+rm ~/.rvmrc
+ln -s ~/.dotfiles/rvmrc ~/.rvmrc
+
+rm ~/.gemrc
+ln -s ~/.dotfiles/gemrc ~/.gemrc
+
+rm ~/.CFUserTextEncoding
+ln -s ~/.dotfiles/CFUserTextEncoding ~/.CFUserTextEncoding
+
+rm ~/.sdirs
+ln -s ~/.dotfiles/sdirs.sh ~/.sdirs
 echo 'Done!'
