beb178642504fcb44370f9730f407556bd86e114
Brian Morton <bmorton@sdreader.com>
2012-01-11 01:49:07 -0800

    Add bash file section headers. Remove some stale exports and reorganize aliases.  Add elastic_search, BOOM, and tc aliases.

--- bash.sh
+++ bash.sh
@@ -1,33 +1,37 @@
-# Completions
+# Completions -------------------------------------------------------------------------------------
 source ~/.dotfiles/bashmarks/bashmarks.sh
 source ~/.dotfiles/git_completion.sh
 
-# Prompt magic
+# Prompt magic ------------------------------------------------------------------------------------
 function parse_git_dirty {
   [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
 }
 
 export PS1='\h:\W$(__git_ps1 "[\[\e[0;32m\]%s\[\e[0m\]\[\e[0;33m\]$(parse_git_dirty)\[\e[0m\]]")$ '
 
-# Exports
+# Exports -----------------------------------------------------------------------------------------
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
-export PATH=$PATH:/usr/local/mysql/bin:/usr/local/www/cakephp/cake/console
-export NODE_PATH=/usr/local/lib/node
-#export CC=/usr/bin/gcc-4.2
 export DYLD_LIBRARY_PATH=/usr/local/mysql/lib:$DYLD_LIBRARY_PATH
+export EDITOR='subl -w'
 
-# Aliases
+# Aliases -----------------------------------------------------------------------------------------
+alias ls='ls -FGhl'
+alias la='ls -FGhlA'
+
+# Aliases for services ----------------------------------------------------------------------------
 alias mongo_start="mongod run --config /usr/local/Cellar/mongodb/2.0.0-x86_64/mongod.conf"
 alias redis_start="redis-server /usr/local/etc/redis.conf"
 alias postgres_start="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
 alias postgres_stop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
-alias ls='ls -FGhl'
-alias la='ls -FGhlA'
-alias push_platypus="cd /Volumes/1703india/Projects/kitchensink && knife ssh 'role:platypus_web_unicorn' 'sudo chef-client' -a ec2.public_hostname -x ec2-user"
-alias fs='foreman start'
+alias elastic_start="elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch/0.16.2/config/elasticsearch.yml"
 
-# External crap
+# Aliases for Ruby stuff --------------------------------------------------------------------------
+alias fs='foreman start'
+alias BOOM='rake db:reset; rake db:test:prepare'
+alias tc='thoughtcrime start'
+
+# External crap -----------------------------------------------------------------------------------
 source ~/.dotfiles/aws.sh
 
 # RVM last!
